30a1f93da074b551dc4c5ea401b036567eaef387
Leon Tzu Chiang Chan <leontzuchiangchan@Leons-Air.lan>
2023-02-03 15:29:20 -0500

    add infos

---
+++ src/name.js
@@ -0,0 +1,22 @@
+const wonders = [
+  {
+    id: 1,
+    name: "Great Pyramid of Giza",
+    construction: "2584-2561 BC",
+    buliders: "Egyptians",
+    destruction: "N/A",
+    cause: "N/A",
+    location: "Egypt"
+  },
+  {
+    id: 2,
+    name: "Hanging Gardens of Babylon",
+    construction: "600 BC",
+    buliders: "Babylonians or Assyrians",
+    destruction: "1 AD",
+    cause: "Unknown",
+    location: "Iraq"
+  },
+];
+
+export default wonders;
